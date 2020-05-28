e3eb01f006c5e8441b615f56a5ba81cc8adfdac0
Yill <hebutyll@outlook.com>
2020-05-28 23:13:15 +0800

    增加AES和DES加密公共方法

---
+++ src/netcore/Encryption/AESEncryption.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NIS.Common.Basic
+{
+    /// <summary>
+    /// AES加密解密封装
+    /// </summary>
+    public static class AESEncryption
+    {
+        /// <summary>
+        /// 将指定的字符串根据key进行加密
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Encrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            Byte[] toEncryptArray = Encoding.UTF8.GetBytes(str);
+
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform cTransform = rm.CreateEncryptor();
+            Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        /// <summary>
+        /// AES解密
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Decrypt(string str, string key)
+        {
+            if (string.IsNullOrEmpty(str)) return null;
+            Byte[] toEncryptArray = Convert.FromBase64String(str);
+
+            RijndaelManaged rm = new RijndaelManaged
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+
+            ICryptoTransform cTransform = rm.CreateDecryptor();
+            Byte[] resultArray = cTransform.TransformFinalBlock(toEncryptArray, 0, toEncryptArray.Length);
+
+            return Encoding.UTF8.GetString(resultArray);
+        }
+    }
+}
---
+++ src/netcore/Encryption/DESEncryption.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NIS.Common.Basic
+{
+    /// <summary>
+    /// DES加密解密
+    /// </summary>
+    public static class DESEncryption
+    {
+        /// <summary>
+        /// DES加密
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Encrypt(string str, string key)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = Encoding.Default.GetBytes(str);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(key);// 密匙
+            des.IV = ASCIIEncoding.ASCII.GetBytes(key);// 初始化向量
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            cs.FlushFinalBlock();
+            var retB = Convert.ToBase64String(ms.ToArray());
+            return retB;
+        }
+
+        /// <summary>
+        /// DES解密
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Decrypt(string str, string key)
+        {
+            DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+            byte[] inputByteArray = Convert.FromBase64String(str);
+            des.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            des.IV = ASCIIEncoding.ASCII.GetBytes(key);
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Write);
+            cs.Write(inputByteArray, 0, inputByteArray.Length);
+            // 如果两次密匙不一样，这一步可能会引发异常
+            cs.FlushFinalBlock();
+            return System.Text.Encoding.Default.GetString(ms.ToArray());
+        }
+    }
+}
